15c24a2708ce3aa84bc305e7c783babad3bebbee
vivijg <57644737+vivijg@users.noreply.github.com>
2020-01-08 21:09:45 +0100

    Add files via upload

---
+++ Programming_foundations/js/script.js
@@ -0,0 +1,56 @@
+// task 1
+var petArray = ["cat", "cow","dog"];
+
+
+
+// task 2
+console.log ("cow");
+
+
+
+// task 3 
+petArray.push("sheep");
+
+
+
+// task 4
+console.log (petArray);
+console.log (petArray.length);
+
+
+// task 5
+var catObject = {
+name: "Goliath",
+colour: "Black",
+age: 9
+}
+
+
+
+// task 6
+console.log (catObject.age)
+
+
+
+//task 7
+var catArray = [catObject];
+console.log (catObject);
+
+
+
+//task 8
+for (var i=0; i <catArray.length; i++) {
+    console.log(catArray[i]);
+};
+
+
+
+//task 9
+function logToConsole(brownGorilla) {
+    console.log(brownGorilla);
+}
+
+
+
+//task 10
+logToConsole("Donkey");
